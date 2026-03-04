5b398e35459ccb695889c448d213ed30f8177530
Yevhen Sydorov <105035206+Sokiryanin@users.noreply.github.com>
2026-03-04 12:10:17 +0100

    add animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,9 @@
 // Підключення функціоналу "Чортоги Фрілансера"
-import { addTouchAttr, addLoadedAttr, isMobile, FLS } from "@js/common/functions.js"
+import {
+  addTouchAttr,
+  addLoadedAttr,
+  isMobile,
+  FLS
+} from '@js/common/functions.js';
+
+addLoadedAttr();
